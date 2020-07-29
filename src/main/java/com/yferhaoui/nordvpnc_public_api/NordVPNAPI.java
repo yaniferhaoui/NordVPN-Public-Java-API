0fa0fcdf648f4a4a25ca502a7c9fac19d12f7293
Yani Ferhaoui <yani.ferhaoui@hotmail.fr>
2020-07-29 23:49:51 +0200

    update

--- src/main/java/com/yferhaoui/nordvpnc_public_api/NordVPNAPI.java
+++ src/main/java/com/yferhaoui/nordvpnc_public_api/NordVPNAPI.java
@@ -188,7 +188,7 @@ public final class NordVPNAPI {
 
 	public final static void main(final String[] args) throws IOException, InterruptedException {
 
-		System.out.println("Server: " + new NordVPNAPI().getRandProxy().getDomain());
+		System.out.println("Server: " + new NordVPNAPI().getRandProxy("US").getDomain());
 
 //		final NordVPNServer[] servers = new NordVPNAPI().getServerList("FR");
 //		int nbProxyServer = 0;
